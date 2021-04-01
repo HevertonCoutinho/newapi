3b1b69abbb3db15f20687917ccfa61775f2070f0
HevertonCoutinho <59829877+HevertonCoutinho@users.noreply.github.com>
2021-04-01 19:34:44 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://heverton:senha@cluster0.zbzix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    connectionString: 'SUA CONNECTION STRING',
     sendgridKey: 'SUA CHAVE SENDGRID KEY',
     containerConnectionString: 'SEU CONTAINER NO AZURE-STORAGE'
 }
